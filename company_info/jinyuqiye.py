6e20d72565b4fccbd01b8973856e57a17aa986ab
myw <mywpython@163.com>
2021-08-01 18:40:22 +0800

    add:form表单bug修复，增加进豫企业spider

--- company_info/jinyuqiye.py
+++ company_info/jinyuqiye.py
@@ -18,25 +18,25 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 from conf import *
 
-class HenanCompy(object):
+class YuCompany(object):
     def __init__(self):
         self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_16_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_16_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3770.80 Safari/537.36',
         }
         self.quali_type = QUALI_TYPE
-        self.re_page_num = re.compile(r'总共.*?>(\d{2,8})<.*?条')
+        self.re_page_num = re.compile(r'总共:[\S\s]*?>(\d{2,8})<.*?条')
         self.form_data_list = self._init_form_data()
-        self.log_file = open(os.path.join(BASE_DIR,'log/form_data.txt'), 'a+', encoding='utf-8')
-        self.err_form_data_file = open(os.path.join(BASE_DIR,'log/err_form_data.txt'), 'a+', encoding='utf-8')
+        self.log_file = open(os.path.join(BASE_DIR,'log/jinyu_form_data.txt'), 'a+', encoding='utf-8')
+        self.err_form_data_file = open(os.path.join(BASE_DIR,'log/jinyu_err_form_data.txt'), 'a+', encoding='utf-8')
         self.err_form_lock = threading.Lock()
-        self.detail_err_file =  open(os.path.join(BASE_DIR,'log/err_detail_url.txt'), 'a+', encoding='utf-8')
-        self.detail_file = open(os.path.join(BASE_DIR,'log/detail_url.txt'), 'a+', encoding='utf-8')
+        self.detail_err_file =  open(os.path.join(BASE_DIR,'log/jinyu_err_detail_url.txt'), 'a+', encoding='utf-8')
+        self.detail_file = open(os.path.join(BASE_DIR,'log/jinyu_detail_url.txt'), 'a+', encoding='utf-8')
         self.log_lock = threading.Lock()
         self.detail_lock = threading.Lock()
         self.q1 = Queue(maxsize=1000)
         self.q2 = Queue(maxsize=1000)
         self.dtail_url_list = self._init_detail_list()
-        self.thread_num = 5
+        self.thread_num = 3
         self.alread_num = 0
         self.err_num = 0
         self.err_form_num = 0
@@ -45,21 +45,20 @@ class HenanCompy(object):
 
     def _init_form_data(self):
         form_list = set()
-        if not os.path.exists(os.path.join(BASE_DIR,'log/form_data.txt')):
+        if not os.path.exists(os.path.join(BASE_DIR,'log/jinyu_form_data.txt')):
             return form_list
-        with open(os.path.join(BASE_DIR,'log/form_data.txt'), 'r', encoding='utf-8') as f:
+        with open(os.path.join(BASE_DIR,'log/jinyu_form_data.txt'), 'r', encoding='utf-8') as f:
             for i in f:
                 form_list.add(i.strip())
         return form_list
 
     def _init_detail_list(self):
         det_url_list = set()
-        if os.path.exists(os.path.join(BASE_DIR,'log/detail_url.txt')):
-            with open(os.path.join(BASE_DIR,'log/detail_url.txt'), 'r', encoding='utf-8') as f:
+        if os.path.exists(os.path.join(BASE_DIR,'log/jinyu_detail_url.txt')):
+            with open(os.path.join(BASE_DIR,'log/jinyu_detail_url.txt'), 'r', encoding='utf-8') as f:
                 for i in f:
                     det_url_list.add(i.strip())
         return det_url_list
-
 
     @retry(stop_max_attempt_number=6, stop_max_delay=3000, wait_fixed=3000, wait_incrementing_increment=500)
     def send_rquest_get(self, url):
@@ -99,68 +98,52 @@ class HenanCompy(object):
             page_num = 0
         return page_num
 
-    def get_detail_url(self,html_str, certtypenum):
+    def get_detail_url(self,html_str):
         html_obj = etree.HTML(html_str)
-        tr_obj_list = html_obj.xpath('//div[@id="tagContenth0"]/table/tbody/tr')[1:]
+        tr_obj_list = html_obj.xpath('//div[@id="tagContenth0"]//table/tbody/tr')[1:-1]
         for tr_obj in tr_obj_list:
             detail_url = 'http://hngcjs.hnjs.gov.cn'+tr_obj.xpath('.//a/@href')[0]
             if detail_url not in self.dtail_url_list:
                 #print('详情页的URL：%s' % detail_url)
-                self.q2.put((detail_url,certtypenum))
+                self.q2.put(detail_url)
 
     def get_info(self):
         form_data = {
-            '__EVENTTARGET':None,
-            '__EVENTARGUMENT': None,
             '__VIEWSTATE': None,
-            'tradetypenum': None,
-            'tradeboundnum': None,
-            '__VIEWSTATEGENERATOR': 'AB12D588',
-            '__EVENTVALIDATION': '/wEdAAnlEV70FrTjlmWb8G2zl/OCcyCwLNtjiGsUsD1klKe8mO/27pz3VRsK7NDdBcWPH1oVz/HNqEavkdJhHEQf0CC9QF5FF4kumzRC1Hm6gbSZLJSStlQIejt9Eiz2dXvmYMkdxEWiDJrToSQwV2qIPrDYCSAehAWh8K/R7+KNdzUNgpSYA6QBBGuDgl6JiXAF2qKw9gjimMdqSvxqmhg71HUxXh4ieFs46FIRmGc4NrfwBw==',
-            'aptScope': None,
-            'aptCode': None,
-            'corpname': None,
-            'corpcode': None,
-            'legalman': None,
-            'certid': None,
-            'site': None,
-            'ry_type': None,
-            'ctl09': '搜索'
+            'page': 1,
+            '__VIEWSTATEGENERATOR': '080F06E4',
+            '__EVENTTARGET': None,
+            '__EVENTARGUMENT': None,
+            '__EVENTVALIDATION': '/wEdAAXsCj80mbqLLeSUnh+3n5ndx7tqcRa6Ih3Kl/7QaECdF7FcoEuNUIffMRwxe8I0FRDU5fYEc164Xu+zZX3GSMqGadUIQrVjT+okoqt9YaNKblYwh47qh7vQax33USSrEVczpqBSGFdPjdb33D70FGcc',
+            'QiYeMingCheng': None,
+            'TongYiSheHuiXinYongDaiMa': None,
+            'QiYeFaDingDaiBiaoRen': None,
+            'QiYeZhuCeShuDi': None
         }
-        for certtypenum in COMPANY_QUALI:
-            form_data['certtypenum'] = int(certtypenum)
-            form_data['page'] = None
-            with self.log_lock:
-                self.log_file.write(hashlib.md5(str(form_data).encode(encoding='UTF-8')).hexdigest()+'\n')
-                self.log_file.flush()
-            html_str = self.send_quest_post(url='http://hngcjs.hnjs.gov.cn/company/list', data=form_data)
-            # 解析第一页的情况
-            self.get_detail_url(html_str, int(certtypenum))
-            page_num = self.get_page_num(html_str)
-            for i in range(2, page_num+1):
-            #for i in range(2, 4):
-                form_data = {
-                    '__EVENTTARGET':None,
-                    '__EVENTARGUMENT': None,
-                    '__VIEWSTATE': None,
-                    'page': i,
-                    'tradetypenum': None,
-                    'tradeboundnum': None,
-                    '__VIEWSTATEGENERATOR': 'AB12D588',
-                    '__EVENTVALIDATION': '/wEdAAnlEV70FrTjlmWb8G2zl/OCcyCwLNtjiGsUsD1klKe8mO/27pz3VRsK7NDdBcWPH1oVz/HNqEavkdJhHEQf0CC9QF5FF4kumzRC1Hm6gbSZLJSStlQIejt9Eiz2dXvmYMkdxEWiDJrToSQwV2qIPrDYCSAehAWh8K/R7+KNdzUNgpSYA6QBBGuDgl6JiXAF2qKw9gjimMdqSvxqmhg71HUxXh4ieFs46FIRmGc4NrfwBw==',
-                    'certtypenum': int(certtypenum),
-                    'aptScope': None,
-                    'aptCode': None,
-                    'corpname': None,
-                    'corpcode': None,
-                    'legalman': None,
-                    'certid': None,
-                    'site': None,
-                    'ry_type': None,
-                }
-                if hashlib.md5(str(form_data).encode(encoding='UTF-8')).hexdigest() in self.form_data_list:
-                    continue
-                self.q1.put(form_data)
+
+        with self.log_lock:
+            self.log_file.write(hashlib.md5(str(form_data).encode(encoding='UTF-8')).hexdigest()+'\n')
+            self.log_file.flush()
+        html_str = self.send_quest_post(url='http://hngcjs.hnjs.gov.cn/jyCorpbasicinfo/list', data=form_data)
+        # 解析第一页的情况
+        self.get_detail_url(html_str)
+        page_num = self.get_page_num(html_str)
+        for i in range(2, page_num+1):
+            form_data = {
+                '__VIEWSTATE': None,
+                'page': i,
+                '__VIEWSTATEGENERATOR': '080F06E4',
+                '__EVENTTARGET': None,
+                '__EVENTARGUMENT': None,
+                '__EVENTVALIDATION': '/wEdAAXsCj80mbqLLeSUnh+3n5ndx7tqcRa6Ih3Kl/7QaECdF7FcoEuNUIffMRwxe8I0FRDU5fYEc164Xu+zZX3GSMqGadUIQrVjT+okoqt9YaNKblYwh47qh7vQax33USSrEVczpqBSGFdPjdb33D70FGcc',
+                'QiYeMingCheng': None,
+                'TongYiSheHuiXinYongDaiMa': None,
+                'QiYeFaDingDaiBiaoRen': None,
+                'QiYeZhuCeShuDi': None
+            }
+            if hashlib.md5(str(form_data).encode(encoding='UTF-8')).hexdigest() in self.form_data_list:
+                continue
+            self.q1.put(form_data)
 
         for i in range(self.thread_num):
             self.q1.put(None)
@@ -177,7 +160,7 @@ class HenanCompy(object):
             if form_data is None:
                 break
             try:
-                html_str = self.send_quest_post(url='http://hngcjs.hnjs.gov.cn/company/list', data=form_data)
+                html_str = self.send_quest_post(url='http://hngcjs.hnjs.gov.cn/jyCorpbasicinfo/list', data=form_data)
             except Exception:
                 with self.err_form_lock:
                     self.err_form_data_file.write(str(form_data))
@@ -185,13 +168,13 @@ class HenanCompy(object):
                     self.err_form_num += 1
                 self.q1.task_done()
                 continue
-            self.get_detail_url(html_str, form_data['certtypenum'])
+            self.get_detail_url(html_str)
             with self.log_lock:
                 self.log_file.write(hashlib.md5(str(form_data).encode(encoding='UTF-8')).hexdigest()+'\n')
                 self.log_file.flush()
             self.q1.task_done()
         # 每个线程结束的时候放入None
-        self.q2.put((None,None))
+        self.q2.put(None)
 
     def work2(self):
         """
@@ -201,11 +184,12 @@ class HenanCompy(object):
         while True:
             if not self.q2.qsize():
                 continue
-            detail_url, certtypenum = self.q2.get()
+            detail_url = self.q2.get()
             if detail_url is None:
                 break
             try:
                 res = self.send_rquest_get(detail_url)
+                print(detail_url)
             except Exception:
                 with self.err_lock:
                     self.detail_err_file.write(detail_url+'\n')
@@ -213,9 +197,9 @@ class HenanCompy(object):
                     self.err_num += 1
                 self.q2.task_done()
                 continue
-            # 文件名字为 网站的ID、certtypenum 的拼接
-            file_name = detail_url.split('=')[1]+'__'+str(certtypenum)+'.html'
-            file_path = os.path.join(BASE_DIR, 'data', file_name)
+            # 文件名字为 公司社会信用代码
+            file_name = detail_url.split('=')[-1]+'.html'
+            file_path = os.path.join(BASE_DIR, 'jinyu_data', file_name)
             with open(file_path, 'w', encoding='utf-8') as f:
                 f.write(res)
 
@@ -247,8 +231,9 @@ class HenanCompy(object):
         print('详情页采集数量：%s \n 详情页出错数量：%s \n 列表页出错数量：%s ' % (self.alread_num, self.err_num, self.err_form_num))
 
 def process():
-    henan = HenanCompy()
-    henan.run()
+    yucom = YuCompany()
+    yucom.run()
+    #yucom.get_info()
 
 if __name__ == '__main__':
     process()
